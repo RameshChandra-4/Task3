0b89433b59fb6bd38d7b8e76b54a1749ef925ac4
Ramesh Chandra Reddy <48215608+RameshChandra-4@users.noreply.github.com>
2019-03-19 09:47:39 +0530

    Update SimpleCompoundInterestTest.java

--- SimpleCompoundInterest/SimpleCompoundInterestTest.java
+++ SimpleCompoundInterest/SimpleCompoundInterestTest.java
@@ -10,17 +10,17 @@
 	@Test
 	public void UnitTestCaseSimpleInterest() {
 		SimpleCompoundInterest simplecompoundinterest;
-		 simpleInterest = new SimpleCompoundInterest(20202, 2.5f, 3);
-		assertEquals(1515.1500244140625, simpleInterest.SimpleInterest());
-		simpleInterest = new SimpleCompoundInterest(20202, 2.5f, 5);
-		assertEquals(2525.25, simpleInterest.SimpleInterest());
+		 simpleInterest = new SimpleCompoundInterest(30301, 2.5f, 3);
+		assertEquals(2272.58, simpleInterest.SimpleInterest());
+		simpleInterest = new SimpleCompoundInterest(56890, 1.7f, 6);
+		assertEquals(5802.78, simpleInterest.SimpleInterest());
 	}
 	@Test
 	void UnitTestCaseCompoundIntrest() {
 		SimpleCompoundInterest compoundInterest;
-		compoundInterest = new SimpleCompoundInterest(20202, 2.5f, 3);
-		assertEquals(21755.342888139312, compoundInterest.CompoundInterest());
-		compoundInterest = new SimpleCompoundInterest(20202, 2.5f, 5);
-		assertEquals(22856.7060585414, compoundInterest.CompoundInterest());
+		compoundInterest = new SimpleCompoundInterest(10000, 2.5f, 3);
+		assertEquals(10768.91, compoundInterest.CompoundInterest());
+		compoundInterest = new SimpleCompoundInterest(20000, 3.7f, 5);
+		assertEquals(23984.12, compoundInterest.CompoundInterest());
 	}
  }
